f56221afe759ff95b0c64a91ff1fbf9e62501349
bhagya2002 <bhagya02.p@gmail.com>
2022-05-27 21:23:37 -0600

    classes

--- udemy/classes-interface/src/app.ts
+++ udemy/classes-interface/src/app.ts
@@ -1,1 +1,20 @@
-// Code goes here!
+class Department {
+  name: string = "DEFAULT";
+
+  //   init work when instance is created
+  constructor(n: string) {
+    this.name = n;
+  }
+
+  describe(this: Department) {
+    console.log("department: " + this.name);
+  }
+}
+
+// instance of Department
+const hr = new Department("HR");
+
+hr.describe();
+
+const accountingCopy = { name: "DUMMY", describe: hr.describe };
+accountingCopy.describe();
