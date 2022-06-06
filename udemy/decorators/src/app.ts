05654d7b3973c206c070114b658f2d149fba4de9
bhagya2002 <bhagya02.p@gmail.com>
2022-06-06 16:39:09 -0600

    decorators for function use

---
+++ udemy/decorators/src/app.ts
@@ -0,0 +1,32 @@
+function Logger(logString: string) {
+  return function (constructor: Function) {
+    console.log(logString);
+    console.log(constructor);
+  };
+}
+
+function WithTemplate(template: string, hookId: string) {
+  return function (constructor: any) {
+    const hookEl = document.getElementById(hookId);
+    const p = new constructor();
+
+    if (hookEl) {
+      hookEl.innerHTML = template;
+      hookEl.querySelector("h1")!.textContent = p.name;
+    }
+  };
+}
+
+// @Logger("Logging - Person constructor")
+@WithTemplate("<h1>My Person Object</h1>", "app")
+class Person {
+  name = "bhagya";
+
+  constructor() {
+    console.log("creating person object");
+  }
+}
+
+const pers = new Person();
+
+console.log(pers);
